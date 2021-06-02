bafea464d19520cb3c0ecfaa839f45cc6503d48f
Sandor Turanszky <sandor.turanszky@gmail.com>
2021-06-02 15:50:25 +0200

    Update tab title

--- src/components/questions/Tabs.js
+++ src/components/questions/Tabs.js
@@ -5,7 +5,7 @@ export default ({ total }) => (
   <div className="flex justify-between items-center my-4 pt-4">
     <ul className="flex text-sm text-medium-on-surface-0">
       <li className="cursor-pointer">
-        <Link to="/questions/active" className="px-1" activeClassName="border-b-2 border-accent">All</Link>
+        <Link to="/questions/active" className="px-1" activeClassName="border-b-2 border-accent">Active</Link>
       </li>
       <li className="cursor-pointer px-4">
         <Link to="/questions/recent" className="px-1" activeClassName="border-b-2 border-accent">Most recent</Link>
